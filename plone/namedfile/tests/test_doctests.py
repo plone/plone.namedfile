0c37b046d5f54f1665cd720f7d0964eeb5cedff4
David Glick <davidglick@groundwire.org>
2011-04-30 03:38:06 +0000

    use stdlib doctest instead of the deprecated one in zope.testing
    svn path=/plone.namedfile/trunk/; revision=49176

--- plone/namedfile/tests/test_doctests.py
+++ plone/namedfile/tests/test_doctests.py
@@ -1,20 +1,20 @@
+import doctest
 import unittest
-from zope.testing import doctestunit
 from plone.namedfile.tests.base import setUp
 from zope.component.testing import tearDown
 
 def test_suite():
     return unittest.TestSuite([
 
-        doctestunit.DocFileSuite(
+        doctest.DocFileSuite(
             'usage.txt', package='plone.namedfile',
             setUp=setUp, tearDown=tearDown),
         
-        doctestunit.DocFileSuite(
+        doctest.DocFileSuite(
             'handler.txt', package='plone.namedfile',
             setUp=setUp, tearDown=tearDown),
         
-        doctestunit.DocFileSuite(
+        doctest.DocFileSuite(
             'marshaler.txt', package='plone.namedfile',
             setUp=setUp, tearDown=tearDown),
         
