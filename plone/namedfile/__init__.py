7ac764d530b1b6d74f172010d93f8368659cc4d7
David Glick <dglick@gmail.com>
2012-05-20 12:03:10 -0400

    I'll probably regret this later

--- plone/namedfile/__init__.py
+++ plone/namedfile/__init__.py
@@ -1,2 +1,9 @@
 from plone.namedfile.file import NamedFile, NamedImage
 from plone.namedfile.file import NamedBlobFile, NamedBlobImage
+
+# BBB alias for zope.app.file.file.FileChunk
+try:
+	import zope.app.file
+except ImportError:
+	import sys
+	sys.modules['zope.app.file'] = sys.modules['plone.namedfile']
