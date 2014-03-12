aa57e1ebe42c43a57a7b045bb22855c28fd77b14
Nathan Van Gheem <vangheem@gmail.com>
2014-03-11 21:32:51 -0500

    disable csrf protection on generating scales

--- plone/namedfile/scaling.py
+++ plone/namedfile/scaling.py
@@ -14,6 +14,8 @@ from zope.interface import implements
 from zope.traversing.interfaces import ITraversable, TraversalError
 from zope.publisher.interfaces import IPublishTraverse, NotFound
 from zope.app.file.file import FileChunk
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
 
 _marker = object()
 
@@ -296,8 +298,11 @@ class ImageScaling(BrowserView):
             if not scale in available:
                 return None
             width, height = available[scale]
-        storage = AnnotationStorage(self.context, self.modified)
 
+        if self.request is not None:
+            alsoProvides(self.request, IDisableCSRFProtection)
+
+        storage = AnnotationStorage(self.context, self.modified)
         info = storage.scale(factory=self.create,
                              fieldname=fieldname,
                              height=height,
