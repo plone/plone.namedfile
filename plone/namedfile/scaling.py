18eaf216fbc8bde70eefdbabc98bce818867af74
Johannes Raggam <raggam-nl@adm.at>
2015-06-11 18:48:22 +0200

    Don't fail, when accessing the tag method of the @@images view, if scale returns None.

--- plone/namedfile/scaling.py
+++ plone/namedfile/scaling.py
@@ -321,4 +321,4 @@ class ImageScaling(BrowserView):
             direction='thumbnail',
             **kwargs):
         scale = self.scale(fieldname, scale, height, width, direction)
-        return scale.tag(**kwargs)
+        return scale.tag(**kwargs) if scale else None
