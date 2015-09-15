a7ccfca34ab85ad792eac26337051a4688a46acf
ezvirtual <ezvirtual@thevirtual.co.nz>
2015-09-15 14:39:25 +1200

    Add functionality to automatic rotate images based on exif data & subscribers for archetypes and dexterity image types on object created or modified

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(name='plone.namedfile',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          'exifread',
           'setuptools',
           'zope.browserpage',
           'zope.component',
