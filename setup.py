9301474ecc00334e4418bdcc5dbea4bb4e236f55
Maurits van Rees <maurits@vanrees.org>
2016-06-08 23:11:06 +0200

    Added classifiers for Plone 4.3 and Python 2.6.
    Since plone 4.3.8 we are using plone.namedfile 3.0.x.
    
    [ci skip]

--- setup.py
+++ setup.py
@@ -20,8 +20,10 @@ setup(
     long_description=long_description,
     classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
