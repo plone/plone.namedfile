27660494c7fab20314797561984229cfa3216233
Johannes Raggam <thetetet@gmail.com>
2017-04-26 00:55:26 +0200

    Fix #46, when tiff_utils.process_tiff failed and a width referenced before assignment error occured.

--- plone/namedfile/utils/png_utils.py
+++ plone/namedfile/utils/png_utils.py
@@ -9,7 +9,9 @@ log = getLogger(__name__)
 
 
 def process_png(data):
-    content_type, w, h = None, -1, -1
+    content_type = None
+    w = -1
+    h = -1
     size = len(data)
     # See PNG 2. Edition spec (http://www.w3.org/TR/PNG/)
     # Bytes 0-7 are below, 4-byte chunk length, then 'IHDR'
@@ -20,15 +22,13 @@ def process_png(data):
     ):
         content_type = 'image/png'
         w, h = struct.unpack('>LL', data[16:24])
-        width = int(w)
-        height = int(h)
 
     # Maybe this is for an older PNG version.
     elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
         # Check to see if we have the right content type
         content_type = 'image/png'
         w, h = struct.unpack('>LL', data[8:16])
-        width = int(w)
-        height = int(h)
 
+    width = int(w)
+    height = int(h)
     return content_type, width, height
