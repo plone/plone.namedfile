b22168d770a2bfcc0cd400d939e507ec42a60529
mauro <mauro.amico@unibo.it>
2020-01-06 12:58:37 +0100

    implements if-range

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'piexif',
+        'plone.app.caching',
         'plone.rfc822>=2.0a1',
         'plone.scale[storage] >=1.4.999',
         'plone.schemaeditor',
