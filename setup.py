9a1aa3517dd41c963981ef029120051767598224
Maurits van Rees <maurits@vanrees.org>
2022-02-23 23:43:07 +0100

    Back to development: 6.0.0a3
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '6.0.0a2'
+version = '6.0.0a3.dev0'
 
 description = 'File types and fields for images, files and blob files with ' \
               'filenames'
