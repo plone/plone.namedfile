4e855f728e543c9fe3c3a90620d0581e203b23ba
Jens W. Klein <jk@kleinundpartner.at>
2022-08-18 16:01:58 +0200

    move getAllowedSizes + getQuality from CMFPlone.utils to here

--- plone/namedfile/utils/__init__.py
+++ plone/namedfile/utils/__init__.py
@@ -337,3 +337,30 @@ def getHighPixelDensityScales():
             {"scale": 3, "quality": settings.quality_3x},
         ]
     return []
+
+def getAllowedSizes():
+    registry = queryUtility(IRegistry)
+    if not registry:
+        return None
+    settings = registry.forInterface(
+        IImagingSchema, prefix="plone", check=False)
+    if not settings.allowed_sizes:
+        return None
+    sizes = {}
+    for line in settings.allowed_sizes:
+        line = line.strip()
+        if line:
+            name, width, height = pattern.match(line).groups()
+            name = name.strip().replace(' ', '_')
+            sizes[name] = int(width), int(height)
+    return sizes
+
+
+def getQuality():
+    registry = queryUtility(IRegistry)
+    if registry:
+        settings = registry.forInterface(
+            IImagingSchema, prefix="plone", check=False)
+        return settings.quality or QUALITY_DEFAULT
+    return QUALITY_DEFAULT
+
