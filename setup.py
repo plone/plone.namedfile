2a50173cf5f3ce09863d59d61abe9ceddbbaaf8f
Maurits van Rees <maurits@vanrees.org>
2022-10-03 10:46:11 +0200

    Update classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
