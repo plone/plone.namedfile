bbfb3c7e10714252f7a912e917759448b608256b
Maurits van Rees <maurits@vanrees.org>
2023-03-14 23:27:40 +0100

    Add python_requires 3.7+.
    [ci skip]

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.7",
     install_requires=[
         "persistent",
         "piexif",
