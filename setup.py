addcd50fd35b13a4041f8c19f2db65fbccd1ebe4
Maurits van Rees <maurits@vanrees.org>
2024-03-15 17:14:06 +0100

    Back to development: 5.7.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '5.7.0'
+version = '5.7.1.dev0'
 
 description = 'File types and fields for images, files and blob files with ' \
               'filenames'
