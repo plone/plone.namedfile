5913d23ff400d0fa1165674b790ee8b66fec3c4d
Maurits van Rees <maurits@vanrees.org>
2026-01-12 10:21:51 +0100

    Back to development: 7.3.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "7.3.0"
+version = "7.3.1.dev0"
 
 description = "File types and fields for images, files and blob files with filenames"
 long_description = (
