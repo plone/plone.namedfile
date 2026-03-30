d98b6c51da1d47b628dae3cbdd0b2ff4d7e27b54
Maurits van Rees <maurits@vanrees.org>
2026-03-30 10:55:27 +0200

    Back to development: 8.0.0a5
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from setuptools import setup
 
-version = "8.0.0a4"
+version = "8.0.0a5.dev0"
 
 description = "File types and fields for images, files and blob files with filenames"
 long_description = (
